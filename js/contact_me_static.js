170d647edce79bc961edce32dedb826aa3abb4f0
sarthak bansal <39374452+sarthak143@users.noreply.github.com>
2020-02-28 19:34:38 +0530

    Update contact_me_static.js

--- js/contact_me_static.js
+++ js/contact_me_static.js
@@ -1,4 +1,4 @@
-$(function() {
+/* $(function() {
 
     $("input,textarea").jqBootstrapValidation({
         preventSubmit: true,
@@ -17,7 +17,8 @@ $(function() {
     });
 });
 
-/*When clicking on Full hide fail/success boxes */
+/*When clicking on Full hide fail/success boxes 
 $('#name').focus(function() {
     $('#success').html('');
 });
+*/
