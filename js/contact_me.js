08801d09ad5d49f57d7d81107e749715b0045f30
sarthak bansal <39374452+sarthak143@users.noreply.github.com>
2020-02-28 19:38:58 +0530

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,4 +1,4 @@
-/*$(function() {
+$(function() {
 
     $("input,textarea").jqBootstrapValidation({
         preventSubmit: true,
@@ -64,8 +64,8 @@
 });
 
 
-/*When clicking on Full hide fail/success boxes 
+/*When clicking on Full hide fail/success boxes  */ 
 $('#name').focus(function() {
     $('#success').html('');
 });
- */ 
+
